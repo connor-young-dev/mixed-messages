c088349ffdc48efdf8ed0950b0d9b460bf956ce1
Connor Young <connor-young-dev@gmail.com>
2022-09-07 09:15:20 +0100

    store message components in object

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("hello world.");
+const components = {
+    greeting: ['Hello', 'Heyy', 'Eyup', 'Hiya', 'Hi', 'Sup', 'Wazzup'],
+    compliment: ['beautiful', 'gorgeous', 'babe', 'babey', 'pretty thang', 'you absolute specimen', 'fitty'],
+    activity: ['watch a movie', 'watch some TV', 'to the cinema', 'for a walk', 'for a quick drink', 'play some pool', 'play the playstation', 'out for a meal']
+}
