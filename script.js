2b00bb6197e0b40040ff2aead842f85b1159eb80
Connor Young <connor-young-dev@gmail.com>
2022-09-07 09:34:34 +0100

    add getter methods for message components

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const components = {
-    greeting: ['Hello', 'Heyy', 'Eyup', 'Hiya', 'Hi', 'Sup', 'Wazzup'],
-    compliment: ['beautiful', 'gorgeous', 'babe', 'babey', 'pretty thang', 'you absolute specimen', 'fitty'],
-    activity: ['watch a movie', 'watch some TV', 'to the cinema', 'for a walk', 'for a quick drink', 'play some pool', 'play the playstation', 'out for a meal']
+    greetings: ['Hello', 'Heyy', 'Eyup', 'Hiya', 'Hi', 'Sup', 'Wazzup'],
+    compliments: ['beautiful', 'gorgeous', 'babe', 'babey', 'pretty thang', 'you absolute specimen', 'fitty'],
+    activitys: ['watch a movie', 'watch some TV', 'to the cinema', 'for a walk', 'for a quick drink', 'play some pool', 'play the playstation', 'out for a meal'],
+    get greeting() {
+        return random(this.greetings);
+    },
+    get compliment() {
+        return random(this.compliments);
+    },
+    get activity() {
+        return random(this.activitys);
+    }
 }
 
 const random = arr => {
