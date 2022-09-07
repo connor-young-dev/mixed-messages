3cfc446b42605803f5a797fd2b2e5af0ba1b355c
Connor Young <connor-young-dev@gmail.com>
2022-09-07 09:24:29 +0100

    random element from array function

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const components = {
     compliment: ['beautiful', 'gorgeous', 'babe', 'babey', 'pretty thang', 'you absolute specimen', 'fitty'],
     activity: ['watch a movie', 'watch some TV', 'to the cinema', 'for a walk', 'for a quick drink', 'play some pool', 'play the playstation', 'out for a meal']
 }
+
+const random = arr => {
+    const item = arr[Math.floor(Math.random()*arr.length)];
+    return item;
+}
